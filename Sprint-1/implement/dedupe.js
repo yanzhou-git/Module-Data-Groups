1d1a2c8f7c75bbf0bc0ee20d81a8a87ce4421274
Yan <nubelaprofesora2021@gmail.com>
2026-01-11 10:57:18 +0100

    implement dedupe

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  return arr.filter((item, index) => arr.indexOf(item) === index);
+}
+
+module.exports = dedupe;
