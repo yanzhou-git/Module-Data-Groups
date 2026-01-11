d0a61c7e259a3ece9a6a78d3102870795c73188e
Yan <nubelaprofesora2021@gmail.com>
2026-01-11 16:09:55 +0100

    refactor

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,8 +1,7 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
+  for (const element of list) {
     if (element === target) {
       return true;
     }
